eb5ab240fed84113c0239a4eabcf207e8ec0a180
Devashish Sharma <devashishsharma5821>
2022-09-28 16:22:15 +0530

    Add to Cart is f(n) is done..!

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,21 +2,75 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   items: [],
-  totalAmount : 0
+  totalAmount: 0,
 };
 
 const cart = createSlice({
   name: "cart",
   initialState,
   reducers: {
-
     addItemsToCart(state, action) {
-      state.items = [action.payload, ...state.items];
+      state.totalAmount = state.totalAmount + Number(action.payload.amount);
+
+      let newItem = true;
+      console.log(state.items);
+      state.items = state.items.map((item) => {
+        if (item.id === action.payload.id) {
+          item.quantity = Number(item.quantity) + 1;
+          newItem = false;
+          return item;
+        }
+        return item;
+      });
+      if (newItem) {
+        state.items = [action.payload, ...state.items];
+      }
     },
 
     removeItemsFromCart(state, action) {
+      state.totalAmount = state.totalAmount - Number(action.payload.amount);
+
+      let newItem = true;
+      state.items = state.items.map((item) => {
+        if (item.id === action.payload.id) {
+          if (item.quantity > 1) {
+            item.quantity = Number(item.quantity) - 1;
+            newItem = false;
+            return item;
+          }
+        }
+        return item;
+      });
+
+      // hell
+      if (newItem) {
+        let num = 0;
+        state.items = state.items.filter((item) => {
+          if (num === 0 && item.id === action.payload.id) {
+            ++num;
+            return !(item.id === action.payload.id);
+          } else if (!(num === 0) && item.id === action.payload.id) {
+            return item.id === action.payload.id;
+          } else {
+            return !(item.id === action.payload.id);
+          }
+        });
+      }
+    },
+
+    removeWholeItems(state, action) {
+      state.totalAmount = state.totalAmount - Number(action.payload.amount);
+
+      let num = 0;
       state.items = state.items.filter((item) => {
-        return !(item.id === action.payload);
+        if (num === 0 && item.id === action.payload.id) {
+          ++num;
+          return !(item.id === action.payload.id);
+        } else if (!(num === 0) && item.id === action.payload.id) {
+          return item.id === action.payload.id;
+        } else {
+          return !(item.id === action.payload.id);
+        }
       });
     },
   },
--- src/components/Cart/ProductsCartItemFrame.js
+++ src/components/Cart/ProductsCartItemFrame.js
@@ -4,11 +4,19 @@ import image1 from "../../Image/HeaderLayoutDetail/image1.png";
 import { useDispatch } from "react-redux";
 import { CartAction } from "../../Store";
 
-const ProductsCartItemFrame = ({ img, id, title, brand, amount }) => {
+const ProductsCartItemFrame = ({ img, id, title, brand, amount, quantity }) => {
   const dispatch = useDispatch();
 
-  const removeFromCartHandler = () => {
-    dispatch(CartAction.removeItemsFromCart(id));
+  const removeFromCartHandler = (id, amount) => {
+    dispatch(CartAction.removeItemsFromCart({ id, amount }));
+  };
+
+  const removeWholeFromCartHandler = (id, amount) => {
+    dispatch(CartAction.removeWholeItems({ id, amount }));
+  };
+
+  const addToCartHandler = (item) => {
+    dispatch(CartAction.addItemsToCart(item));
   };
 
   return (
@@ -16,17 +24,33 @@ const ProductsCartItemFrame = ({ img, id, title, brand, amount }) => {
       <div className={classes.subContainer1}>
         <img src={img} className={classes.subContainer1sub1} alt={brand} />
         <div className={classes.subContainer1sub2}>
-          <button className={classes.subcontainer1sub2btn1}>-</button>
-          <button className={classes.subcontainer1sub2btn2}>1</button>
-          <button className={classes.subcontainer1sub2btn3}>+</button>
+          <button
+            className={classes.subcontainer1sub2btn1}
+            onClick={removeFromCartHandler.bind(null, id, amount)}
+          >
+            -
+          </button>
+          <button className={classes.subcontainer1sub2btn2}>{quantity}</button>
+          <button
+            className={classes.subcontainer1sub2btn3}
+            onClick={addToCartHandler.bind(null, {
+              img,
+              id,
+              title,
+              brand,
+              amount,
+            })}
+          >
+            +
+          </button>
         </div>
       </div>
       <div className={classes.subContainer2}>
         <p className={classes.subContainer2p1}>{title}</p>
-        <p className={classes.subContainer2p2}>
+        <div className={classes.subContainer2p2}>
           <p>Seller:RetailNet</p>
           <img src={image1} alt={brand} />
-        </p>
+        </div>
         <p className={classes.subContainer2p3}>
           <span className={classes.subContainer2p3s1}>₹{amount}</span>
           <span className={classes.subContainer2p3s2}>₹499</span>
@@ -34,7 +58,7 @@ const ProductsCartItemFrame = ({ img, id, title, brand, amount }) => {
         </p>
         <button
           className={classes.subContainer2p4}
-          onClick={removeFromCartHandler.bind(null,id)}
+          onClick={removeWholeFromCartHandler.bind(null, id, amount)}
         >
           REMOVE
         </button>
